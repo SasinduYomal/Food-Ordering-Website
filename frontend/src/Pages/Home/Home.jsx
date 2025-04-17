d367b88f77c42c359b6c50012f7ace719e2ca4d4
Sasindu Yomal <sasinduyomal2002@gmail.com>
2025-04-17 23:05:32 +0530

    edit the home page

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
+
+
+
+  const [category, setCategory] = React.useState("All")
   return (
     <div >
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }

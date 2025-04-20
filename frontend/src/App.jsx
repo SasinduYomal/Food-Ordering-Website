7a6b761eecb8d1a0711c8c568ac204fa3886b70d
Sasindu Yomal <sasinduyomal2002@gmail.com>
2025-04-20 20:58:59 +0530

    full complete the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,18 @@ import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 import Footer from "./Components/Footer/Footer";
+import LoginPopup from "./Components/LoginPopup/LoginPopup";
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = React.useState(false);
+
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className="App">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
